8d76fe46809c09815047dec63abec83772173735
Farhan <muhammadfarhan@staff.unsia.ac.id>
2024-09-02 16:22:12 +0700

    optimasi

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./components/Login";
 import Users from "./pages/Users";
-import Products from "./pages/Products";
-import AddUser from "./pages/AddUser";
-import EditUser from "./pages/EditUser";
-import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
 import MateriList from "./pages/Materi";
 import TugasList from "./pages/Tugas";
 import MateriMahasiswa from "./pages/Mahasiswa/MahasiswaMateri"
@@ -21,11 +16,6 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
-          <Route path="/users/add" element={<AddUser />} />
-          <Route path="/users/edit/:id" element={<EditUser />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/add" element={<AddProduct />} />
-          <Route path="/products/edit/:id" element={<EditProduct />} />
           <Route path="/materi" element={<MateriList />} />
           <Route path="/tugas" element={<TugasList />} />
           <Route path="/canvas" element={<Canvas />} />
