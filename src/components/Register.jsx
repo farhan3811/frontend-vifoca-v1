2166b3868c374bfcd161b3765358d22ce0eab849
Farhan <muhammadfarhan@staff.unsia.ac.id>
2024-10-03 13:45:19 +0700

    register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 import {
   Card,
   CardBody,
@@ -10,12 +10,12 @@ import {
 } from "@material-tailwind/react";
 
 const RegisterPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [nim, setNim] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [nim, setNim] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -25,9 +25,10 @@ const RegisterPage = () => {
       return;
     }
 
-    const API_URL = process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_API_URL_PROD
-      : process.env.REACT_APP_API_URL_LOCAL;
+    const API_URL =
+      process.env.NODE_ENV === "production"
+        ? process.env.REACT_APP_API_URL_PROD
+        : process.env.REACT_APP_API_URL_LOCAL;
 
     try {
       const response = await axios.post(`${API_URL}/register`, {
@@ -35,11 +36,11 @@ const RegisterPage = () => {
         email,
         nim,
         password,
-        role: 'mahasiswa',
+        role: "mahasiswa",
       });
 
       alert(response.data.msg);
-      navigate('/');
+      navigate("/");
     } catch (error) {
       setErrorMessage(error.response?.data?.msg || "An error occurred.");
     }
@@ -51,62 +52,64 @@ const RegisterPage = () => {
         <Card className="lg:w-4/12 py-8 px-4 sm:w-80">
           <div className="text-center">
             <Typography variant="h3" color="black" className="font-title">
-Daftar
+              Daftar
             </Typography>
             <Typography color="black" className="font-title text-sm">
-            Masukkan NPM dan password Anda untuk melakukan pendaftaran
+              Masukkan NPM dan password Anda untuk melakukan pendaftaran
             </Typography>
           </div>
           <CardBody className="flex flex-col">
             <div className="mb-4">
               <Typography className="font-title font-medium">Nama</Typography>
               <label className="input input-bordered flex items-center gap-2 bg-white border-gray-300">
-                <input 
-                  type="text" 
-                  className="grow" 
+                <input
+                  type="text"
+                  className="grow"
                   placeholder="Nama"
-                  value={name} 
+                  value={name}
                   onChange={(e) => setName(e.target.value)}
-                  required 
+                  required
                 />
               </label>
             </div>
             <div className="mb-4">
               <Typography className="font-title font-medium">NPM</Typography>
               <label className="input input-bordered flex items-center gap-2 bg-white border-gray-300">
-                <input 
-                  type="text" 
-                  className="grow bg-white" 
+                <input
+                  type="text"
+                  className="grow bg-white"
                   placeholder="NPM"
-                  value={nim} 
+                  value={nim}
                   onChange={(e) => setNim(e.target.value)}
-                  required 
+                  required
                 />
               </label>
             </div>
             <div className="mb-4">
               <Typography className="font-title font-medium">E-mail</Typography>
               <label className="input input-bordered flex items-center gap-2 bg-white border-gray-300">
-                <input 
-                  type="email" 
-                  className="grow bg-white" 
+                <input
+                  type="email"
+                  className="grow bg-white"
                   placeholder="email@example.com"
-                  value={email} 
+                  value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  required 
+                  required
                 />
               </label>
             </div>
             <div className="mb-4">
-              <Typography className="font-title font-medium">Password</Typography>
+              <Typography className="font-title font-medium">
+                Password
+              </Typography>
               <label className="input input-bordered flex items-center gap-2 bg-white border-gray-300">
-                <input 
-                  type="password" 
-                  className="grow bg-white" 
+                <input
+                  type="password"
+                  className="grow bg-white"
                   placeholder="Password"
-                  value={password} 
+                  value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  required 
+                  required
                 />
               </label>
             </div>
@@ -115,13 +118,13 @@ Daftar
                 Confirm Password
               </Typography>
               <label className="input input-bordered flex items-center gap-2 bg-white border-gray-300">
-                <input 
-                  type="password" 
-                  className="grow bg-white" 
+                <input
+                  type="password"
+                  className="grow bg-white"
                   placeholder="Confirm Password"
-                  value={confirmPassword} 
+                  value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
-                  required 
+                  required
                 />
               </label>
             </div>
@@ -132,20 +135,24 @@ Daftar
             </div>
           )}
           <CardFooter className="pt-0">
-            <Button 
-              onClick={handleSubmit} 
-              className="bg-blue font-title font-medium" 
+            <Button
+              onClick={handleSubmit}
+              className="bg-blue font-title font-medium"
               fullWidth
             >
               Daftar
             </Button>
             <div className="py-3 flex items-center text-sm text-gray-800 before:flex-1 before:border-t before:border-gray-400 before:me-6 after:flex-1 after:border-t after:border-gray-400 after:ms-6 dark:text-white dark:before:border-neutral-600 dark:after:border-neutral-600">
-            Atau
-          </div>
-          <Link to="/">
-          <Button className="bg-white text-black border-2 font-title font-medium" fullWidth>
-            Masuk
-          </Button></Link>
+              Atau
+            </div>
+            <Link to="/">
+              <Button
+                className="bg-white text-black border-2 font-title font-medium"
+                fullWidth
+              >
+                Masuk
+              </Button>
+            </Link>
           </CardFooter>
         </Card>
       </div>
